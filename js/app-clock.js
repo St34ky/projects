e01d9027119ae363f61c1edf34f4304bfb5240c6
Steak <jessy.napolitano@gmail.com>
2019-12-29 15:58:04 +0100

    Issue #1

--- js/app-clock.js
+++ js/app-clock.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+
+const time = new Date();
+
+function currentTime(){
+var hour = time.getHours();
+var minute = time.getMinutes();
+var second = time.getSeconds();
+document.getElementById("hours").innerHTML = hour;
+document.getElementById("mins").innerHTML = minute;
+document.getElementById("secs").innerHTML = second;
+}
+setInterval(currentTime,500);
+
+window.onload = currentTime;
+
+
+
+
+
+
